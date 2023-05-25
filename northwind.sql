016d9b5d704f87f1c76f770a8443af2069577f8d
DomagojM95 <markovicdomagoj1995@gmail.com>
2023-05-25 12:11:23 +0200

    Create northwind.sql

---
+++ northwind.sql
@@ -0,0 +1,9 @@
+create database northwind;
+
+use northwind;
+
+select * from employees where reportsto is null;
+
+select * from products where unitprice <100;
+
+select * from products where productname like '%he%';
