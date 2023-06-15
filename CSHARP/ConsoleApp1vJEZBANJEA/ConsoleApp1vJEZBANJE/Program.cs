c0a46b458bd513023bd3f10556b85a310eed3a54
DomagojM95 <markovicdomagoj1995@gmail.com>
2023-06-15 20:13:45 +0200

    -

---
+++ CSHARP/ConsoleApp1vJEZBANJEA/ConsoleApp1vJEZBANJE/Program.cs
@@ -0,0 +1,84 @@
+﻿
+
+
+
+//int i;
+
+//i = 5;
+
+//string nizZnakova = "edunova";
+
+
+//bool logickaVrijednost=true;
+
+//Console.WriteLine(logickaVrijednost);
+
+
+
+//float decimalniBroj = 3.14f;
+
+//double decimalniBrojVeci = 3.14;
+
+//decimal db = 3.14m;
+
+
+//Console.WriteLine("decimalni broj: {0}\ndb: {1}",
+//    decimalniBroj,db);
+
+
+////Console.WriteLine(int.MinValue);
+////Console.WriteLine(int.MaxValue);
+
+////int ha = 4;
+////string e = "e";
+//////double k = 4.4;
+////bool h = true;
+
+////Console.WriteLine("{0}\n{1}\n{2}\n{3}\n", ha, e, k, h);
+
+
+////int j, i, m = 3;
+
+
+//Console.Write("unesite broj: ");
+
+//int broj=int.Parse(Console.ReadLine());
+
+//broj = broj + 16;
+
+//Console.WriteLine(broj);
+
+
+//Console.Write("unesi prvi broj: ");
+
+//double b1 = double.Parse(Console.ReadLine());
+
+//Console.Write("unesi drugi broj: ");
+
+//double b2 = double.Parse(Console.ReadLine());
+
+//double rez = b1 + b2;
+
+//Console.WriteLine(rez);
+
+
+float f = 7f;
+
+double d = f;
+
+
+
+string s1 = "broj" + 5;
+
+Console.WriteLine(s1);
+
+Console.WriteLine("unesi broj: ");
+
+int negativanBroj = -262;
+
+negativanBroj = -262 * (-1);
+
+
+
+
+ 
