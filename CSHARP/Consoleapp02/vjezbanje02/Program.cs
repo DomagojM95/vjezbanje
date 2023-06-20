d1057734d9d0fda378bcbb61efb7a004cb457a98
DomagojM95 <markovicdomagoj1995@gmail.com>
2023-06-20 12:05:05 +0200

    ,

---
+++ CSHARP/Consoleapp02/vjezbanje02/Program.cs
@@ -0,0 +1,34 @@
+﻿//Console.WriteLine("unesi broj; ");
+
+//int broj = int.Parse(Console.ReadLine());
+
+//if(broj <= 10)
+//{
+//    Console.WriteLine("hehhoh");
+//}
+
+//else
+//{
+//    Console.WriteLine("nenhoho");
+//}
+
+//string nemavezekakosezoveubvijet = "hi";
+
+
+//switch (nemavezekakosezoveubvijet)
+//{
+//    case "ba":
+//         Console.WriteLine("baba");
+//        break;
+//    case "he":
+//        Console.WriteLine("hehe");
+//        break;
+//    case "hi":
+//        Console.WriteLine("hihi");
+//        break;
+//}
+
+
+string[] jedanniz = { "sd", "dasd", "3123123", "kokoko" };
+
+Console.WriteLine(jedanniz[2]);
