eb1f752cb9933782ab51a27aa2a7e6598abb4d62
DomagojM95 <markovicdomagoj1995@gmail.com>
2023-07-31 11:28:46 +0200

    .

---
+++ CSHARP/bzvzvvjezbanej02/bzvzvvjezbanej02/Metode.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Data;
+
+namespace bzvzvvjezbanej02
+{
+    internal class Metode
+    {
+
+        public void ispis()
+        {
+            Console.WriteLine("hello");
+        }
+
+        public void ispis(string poruka)
+        {
+            Console.WriteLine(poruka);
+        }
+
+        public void ispis(int poruka)
+        {
+
+        }
+
+        public static int izracunaj(int a, int b)
+        {
+            return a+b;
+        }
+
+
+        public static void IspisiMatricu(int[,]matrica)
+        {
+            for(int i = 0; i < matrica.GetLength(0); i++)
+            {
+                for(int j = 0;j< matrica.GetLength(1); j++)
+                {
+                    Console.Write(matrica[i,j]+" ");
+
+                }
+                Console.WriteLine();
+            }
+            string s = "";
+            for(int i = 0; i < matrica.GetLength(1) * 2; i++)
+            {
+                s += "*";
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ CSHARP/bzvzvvjezbanej02/bzvzvvjezbanej02/Program.cs
@@ -0,0 +1,79 @@
+﻿
+
+using System.Diagnostics.Tracing;
+using bzvzvvjezbanej02;
+
+void Tip1()
+{
+    Console.WriteLine("Dobrodošli u program");
+}
+
+Tip1();
+
+void Tip2(string poruka)
+{
+    Console.WriteLine(poruka);
+}
+Tip2("ovo je vrijednost koju prosljedujem");
+string s = "vrijednost varijable s";
+
+Tip2(s);
+
+
+int Tip3()
+{
+    return new Random().Next();
+}
+
+Tip3();
+Console.WriteLine(Tip3());
+
+
+int sb = Tip3();
+Console.WriteLine(sb);
+
+
+
+int Tip4(int min , int max)
+{
+    int manji= min <max ? min : max;
+    int veci = max >min ? max : min;
+    sb = 3;
+    return new Random().Next(manji,veci);
+}
+
+
+Console.WriteLine(Tip4(20,30));
+Console.WriteLine(Tip4(-20, -30));
+
+
+
+
+void ispis()
+{
+    Console.WriteLine("HELLO WORLD");
+}
+
+//void ispis(string poruka)
+//{
+
+//}
+
+Metode m= new Metode();
+m.ispis();
+m.ispis("12");
+m.ispis(12);
+
+Console.WriteLine( Metode.izracunaj(23, 12));
+
+
+int[,] t = new int[5, 5];
+
+
+Metode.IspisiMatricu(t);
+
+t[2, 4] = 7;
+
+Metode.IspisiMatricu(t);
+
+//stao na predavanju 28.06, 49 minuta
