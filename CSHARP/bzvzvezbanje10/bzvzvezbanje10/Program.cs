53ae77db311f3586f46663030a81d56b72895cbd
DomagojM95 <markovicdomagoj1995@gmail.com>
2023-08-24 21:24:14 +0200

    .

---
+++ CSHARP/bzvzvezbanje10/bzvzvezbanje10/Program.cs
@@ -0,0 +1,85 @@
+﻿
+
+using System.Threading.Channels;
+using bzvzvezbanje10;
+
+int KlasicnaMetoda(int b)
+{
+    return b * b;
+}
+
+Console.WriteLine(KlasicnaMetoda(5));
+
+var kvadrat = (int b) => b * b;
+
+Console.WriteLine(kvadrat(5));
+
+var algoritam = (int x, int y) =>
+{
+    var t = x++ + --y;
+    return x + y - t;
+};
+
+
+Console.WriteLine(algoritam(1,2));
+
+
+
+int[] brojevi = { 2, 3, 4, 3, 2, 3, 4, 3 };
+
+int tri = 0;
+
+for(int i=0;i<brojevi.Length;i++)
+{
+    if (brojevi[i] == 3)
+    {
+        tri++;  
+    }
+    
+}
+
+Console.WriteLine(tri);
+
+Console.WriteLine(brojevi.Count(b=> b>3));
+
+
+
+for(int i = 0; i < brojevi.Length; i++)
+{
+    Console.WriteLine(brojevi[i]);
+}
+
+Console.WriteLine("---------------------------");
+
+
+foreach (int k in brojevi)
+{
+    Console.WriteLine(k);
+}
+
+Console.WriteLine("-----------------------------");
+
+Array.ForEach(brojevi, Console.WriteLine);
+
+Console.WriteLine("-----------------------------");
+
+Array.ForEach(brojevi, b =>
+{
+    Console.WriteLine(b + 1);
+});
+Console.WriteLine("-----------------------------");
+var lista=new List<int>() { 2,3,4,5,4};
+
+lista.ForEach(Console.WriteLine);
+
+
+
+var smjerovi= new List<Smjer>() { 
+    new () { Naziv="Prvi"},
+    new () { Naziv="Drugi" }
+};
+
+
+smjerovi.ForEach(Console.WriteLine);
+
+
---
+++ CSHARP/bzvzvezbanje10/bzvzvezbanje10/Smjer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bzvzvezbanje10
+{
+    internal class Smjer
+    {
+        public int Sifra { get; set; }
+        public String? Naziv { get; set; }
+
+        public override string ToString()
+        {
+            return Naziv==null?"":Naziv;
+        }
+
+
+    }
+}
+
+
